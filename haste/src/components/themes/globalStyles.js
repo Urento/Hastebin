19fb416ce6c5b4cb8d19bfee083752aec6b1e3e1
Jan Burzinski <jan@burzinski.de>
2021-03-20 14:47:08 +0100

    updated font

--- haste/src/components/themes/globalStyles.js
+++ haste/src/components/themes/globalStyles.js
@@ -3,6 +3,6 @@ export const GlobalStyles = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
-    font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
+    font-family: Roboto, Helvetica, Arial, Roboto, sans-serif;
   }
   `;
