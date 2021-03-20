f5d5f42422947402101359670ea8af916858ea17
Jan Burzinski <jan@burzinski.de>
2021-03-20 12:19:23 +0100

    added darkmode

--- haste/src/components/Navbar.js
+++ haste/src/components/Navbar.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar, Nav, Form, Button } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import ViewHaste from "./haste/ViewHaste";
 import App from "../App";
+import { ThemeProvider } from "styled-components";
+import Toggle from "./Toggler";
+import { GlobalStyles } from "./globalStyles";
+import { lightTheme, darkTheme } from "./Themes";
+import { useDarkMode } from "./useDarkMode";
 
-export default class NavbarTemplate extends React.Component {
-  render() {
-    return (
-      <Router>
+const NavbarTemplate = () => {
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
+  if (!mountedComponent) return <div />;
+  return (
+    <Router>
+      {theme === "light" ? (
         <Navbar bg="light" expand="lg">
           <Navbar.Brand href="/">Hastebin</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -15,18 +23,49 @@ export default class NavbarTemplate extends React.Component {
             <Nav className="mr-auto">
               <Nav.Link href="/">New Haste</Nav.Link>
             </Nav>
-            <Form inline>
-              <Button variant="outline-success">LOGIN</Button>
-            </Form>
+            <ThemeProvider theme={themeMode}>
+              <>
+                <GlobalStyles />
+                <div className="App">
+                  <Toggle theme={theme} toggleTheme={themeToggler} />
+                </div>
+              </>
+            </ThemeProvider>
           </Navbar.Collapse>
         </Navbar>
+      ) : (
+        <Navbar bg="dark" expand="lg">
+          <Navbar.Brand href="/" style={{ color: "white" }}>
+            Hastebin
+          </Navbar.Brand>
+          <Navbar.Toggle
+            style={{ color: "white" }}
+            aria-controls="basic-navbar-nav"
+          />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/" style={{ color: "white" }}>
+                New Haste
+              </Nav.Link>
+            </Nav>
+            <ThemeProvider theme={themeMode}>
+              <>
+                <GlobalStyles />
+                <div className="App">
+                  <Toggle theme={theme} toggleTheme={themeToggler} />
+                </div>
+              </>
+            </ThemeProvider>
+          </Navbar.Collapse>
+        </Navbar>
+      )}
+      <Switch>
         <Switch>
-          <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/v/:id" component={ViewHaste} />
-          </Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/v/:id" component={ViewHaste} />
         </Switch>
-      </Router>
-    );
-  }
-}
+      </Switch>
+    </Router>
+  );
+};
+export default NavbarTemplate;
--- server/controller/index.controller.js
+++ server/controller/index.controller.js
@@ -1,39 +1,38 @@
-'use strict';
+"use strict";
 
-var mongoose = require('mongoose'),
-  Task = mongoose.model('haste');
+var mongoose = require("mongoose"),
+  Task = mongoose.model("haste");
 
-exports.list_all_hastes = function(req, res) {
-  Task.find({}, function(err, customers) {
-    if (err)
-      res.send(err);
+exports.list_all_hastes = function (req, res) {
+  Task.find({}, function (err, customers) {
+    if (err) res.send(err);
     res.json(customers);
   });
 };
 
-exports.create_a_haste = function(req, res) {
+exports.create_a_haste = function (req, res) {
   var new_task = new Task(req.body);
-  new_task.save(function(err, task) {
-    if (err)
-      res.send(err);
+  new_task.save(function (err, task) {
+    if (err) res.send(err);
     res.json(task);
   });
 };
 
-exports.get_haste_byid = function(req, res) {
-  Task.findOne({id: req.params.id}, function(err, customerInfo) {
-    if (err)
-      res.send(err);
+exports.get_haste_byid = function (req, res) {
+  Task.findOne({ id: req.params.id }, function (err, customerInfo) {
+    if (err) res.send(err);
     res.json(customerInfo);
   });
 };
 
-exports.delete_a_haste = function(req, res) {
-  Task.remove({
-    id: req.params.id
-  }, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'Haste was successfully deleted' });
-  });
+exports.delete_a_haste = function (req, res) {
+  Task.remove(
+    {
+      id: req.params.id,
+    },
+    function (err, task) {
+      if (err) res.send(err);
+      res.json({ message: "Haste was successfully deleted" });
+    }
+  );
 };
